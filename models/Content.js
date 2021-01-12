eec9ea3cd7d4b5ba03b45b578a46654dad2cb750
Sunrise Gong <rhdgoehe2@gmail.com>
2021-01-12 14:47:41 +0900

    [Add] task2: connect RDS

---
+++ models/Content.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Content', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    category: {
+      type: DataTypes.STRING(45),
+      allowNull: true
+    },
+    contentlink: {
+      type: DataTypes.STRING(45),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'Content',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
